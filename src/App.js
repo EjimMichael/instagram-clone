fc58591411356c15d0f0b84bac0c7f3e9b0adaca
Ejim Michael <ejimmichael1@gmail.com>
2021-08-20 15:42:44 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+          alt="IG logo"
+        />
+      </div>
+      <h1>React</h1>
+    </div>
+  );
+}
+
+export default App; 
