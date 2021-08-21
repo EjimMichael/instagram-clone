f32961b13871f4538195f9dbe86a3a67af237478
Ejim Michael <ejimmichael1@gmail.com>
2021-08-21 19:42:17 +0100

    Add new document using firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Post from './Post'; 
+import { db } from "./firebase";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username: "Jimmy_Solade",
-      caption: "wow it works",
-      imageUrl:
-        "https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fHdlYnNpdGUlMjBsb2dpbiUyMHBhZ2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    },
-    {
-      username: "Jimmy_Solade",
-      caption: "wow it works",
-      imageUrl:
-        "https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fHdlYnNpdGUlMjBsb2dpbiUyMHBhZ2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) =>  {
+      setPosts(snapshot.docs.map((doc) => doc.data()));
+    })
+  }, [])
 
   return (
     <div className="App">
